3b881a2c7cb55b65c03851b166669a0d62a0c696
Priyanka Shinde <shinde.pri@northeastern.edu>
2025-01-10 02:59:21 -0500

    Config set for tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,27 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "primary-text-color": "#323232",
+        "primary-text-color-70": "rgba(50, 50, 50, 0.7)",
+        "secondary-bg-color": "#F6F6F6",
+        "tertiary-bg-color": "#F6F8FF",
+      },
+      lineHeight: {
+        86: "5.375rem", // 86px = 5.375rem
+      },
+      spacing: {
+        "0.875": "0.875rem", // 14px
+      },
+      fontFamily: {
+        figtree: ["var(--font-figtree)", "sans-serif"],
+        lexend: ["var(--font-lexend)", "sans-serif"],
+      },
+      fontSize: {
+        "40": "40px", // Add custom font size
+        "32": "32px",
+      },
+      letterSpacing: {
+        "22": "0.22em", // 22% letter-spacing
       },
     },
   },
